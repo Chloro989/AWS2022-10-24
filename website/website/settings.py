fbe7cb6c72faf5f16ed71e59543c725440c64b42
Ubuntu <ubuntu@ip-172-31-17-80.ec2.internal>
2023-06-19 17:08:25 +0000

    edited 2

--- website/website/settings.py
+++ website/website/settings.py
@@ -31,7 +31,23 @@ DEBUG = env.bool("DEBUG")
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'debug.log',
+        },
+    },
+    'root': {
+        'handlers': ['file'],
+        'level': 'DEBUG',
+    },
+}
 
 ALLOWED_HOSTS = ["3.94.15.137", "chloro966.net"]
 
